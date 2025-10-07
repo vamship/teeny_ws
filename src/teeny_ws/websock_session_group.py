2055a2af931ad10f27dc2a8c59157eb598c712e4
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2025-10-07 18:08:34 -0400

    Add a small delay in the processing loop for websocket connections

--- src/teeny_ws/websock_session_group.py
+++ src/teeny_ws/websock_session_group.py
@@ -1,70 +1,75 @@
 from teeny_logger import Logger
+
 from .websock_session import WebSockSession
 
-logger = Logger('wseg')
+logger = Logger("wseg")
 _default_group = None
 
 
 class WebSockSessionGroup:
-    """ Class that manages a group of websocket sessions. This method can be
+    """Class that manages a group of websocket sessions. This method can be
     used to invoke periodic methods on each session within the group.
     """
 
-    def __init__(self, name='default') -> None:
-        """ Initializes the session group. """
+    def __init__(self, name="default") -> None:
+        """Initializes the session group."""
         self._name = name
         self._sessions = []
         self._is_running = False
 
     @property
     def is_running(self) -> bool:
-        """ Flag that indicates whether or not the group is running (i.e.,
+        """Flag that indicates whether or not the group is running (i.e.,
         polling and processing requests over one or more websocket sessions)
         """
         return self._is_running
 
     @property
     def name(self) -> str:
-        """ The name of the current group """
+        """The name of the current group"""
         return self._name
 
     def add_connection(self, session: WebSockSession) -> None:
-        """ Adds a new web socket session to the group.
+        """Adds a new web socket session to the group.
 
         :param session: A websocket session object represeting the web socket
         session.
         """
-        logger.info(f'Added new connection [{session.id}]')
+        logger.info(f"Added new connection [{session.id}]")
         session.set_close_handler(self.remove_connection)
         self._sessions.append(session)
 
-    def remove_connection(self,
-                          session: WebSockSession) -> None:
-        """ Removes a WebSocket connection from the manager.
+    def remove_connection(self, session: WebSockSession) -> None:
+        """Removes a WebSocket connection from the manager.
 
         :param session: The web socket session to remove from the group.
         """
-        logger.info(f'Removed connection [{self._name}] [{session.id}]')
+        logger.info(f"Removed connection [{self._name}] [{session.id}]")
         self._sessions.remove(session)
 
     def start(self) -> None:
-        """ Method that periodically invokes the process methods on each of the
+        """Method that periodically invokes the process methods on each of the
         active sessions within the group.
         """
-        logger.info(f'Starting web socket group [{self._name}]')
+        import time
+        logger.info(f"Starting web socket group [{self._name}]")
         self._is_running = True
         while self._is_running:
+            # --- HACK ---
+            # Adding a small sleep here to allow interrupts triggered by other
+            # threads to be processed.
+            time.sleep(0.0005)
             for session in self._sessions:
                 session.process()
 
     def stop(self) -> None:
-        """ Stops processing messages within the group """
-        logger.info(f'Stopping websocket group [{self._name}]')
+        """Stops processing messages within the group"""
+        logger.info(f"Stopping websocket group [{self._name}]")
         self._is_running = False
 
 
 def get_default_group() -> WebSockSessionGroup:
-    """ Returns the default session group. If it does not exist, it will be
+    """Returns the default session group. If it does not exist, it will be
     created.
 
     :return: The default session group.
